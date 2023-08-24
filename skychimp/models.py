a3bfa863f9bac0e69ee4248344a2b92cdb4a3a78
BasilZap <zapr9@yandex.ru>
2023-08-24 23:34:00 +0300

    Созданы основы для моделей, обновлен файл requirements.txt

--- skychimp/models.py
+++ skychimp/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    client_email = models.EmailField(verbose_name='Почтовый адрес', unique=True)
+    full_name = models.CharField(max_length=200, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='Краткий комментарий')
+
+    def __str__(self):
+        return f'{self.full_name}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class MailingConfig(models.Model):
+    STATE_CHOICE = [
+        ('F', 'завершена'),
+        ('R', 'запущена'),
+        ('C', 'создана'),
+    ]
+
+    mailing_time = models.TimeField(verbose_name='Время рассылки', **NULLABLE)
+    mailing_count = models.DurationField(verbose_name='Периодичность рассылки')
+    mailing_state = models.CharField(max_length=1, choices=STATE_CHOICE, default='C', verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.mailing_time} - {self.mailing_count}'
+
+    class Meta:
+        verbose_name = 'Настройка рассылки'
+        verbose_name_plural = 'Настройки рассылки'
+
+
+class UserMail(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Тема письма')
+    body = models.TextField(verbose_name='Тема письма')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Сообщение для рассылки'
+        verbose_name_plural = 'Сообщения для рассылки'
+
+
+class MailingTry(models.Model):
+    try_time = models.DateTimeField(verbose_name='Дата попытки')
+    try_state = models.BooleanField(verbose_name='Статус попытки', **NULLABLE)
+    server_response = models.CharField(max_length='200', verbose_name='Ответ сервера', **NULLABLE)
+
+    def __str__(self):
+        if self.try_state is not None:
+            if self.try_state:
+                state_str = 'Успешно'
+            else:
+                state_str = 'Ошибка'
+        else:
+            state_str = 'Не определено'
+        return f'{self.try_time} - статус: {state_str}'
+
+    class Meta:
+        verbose_name = 'Попытка рассылки'
+        verbose_name_plural = 'Попытки рассылки'

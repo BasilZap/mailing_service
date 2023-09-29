b81ed4457a8121ba37cbc3a92d442035bcaf8c2b
BasilZap <zapr9@yandex.ru>
2023-09-30 00:28:23 +0300

    Сформирована модель users и миграции, созданы контроллеры, шаблоны, формы, прописаны urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.core.mail import send_mail
+from users.forms import UserRegisterForm, UserForm
+from django.urls import reverse_lazy, reverse
+from users.models import User
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('skychimp:list')
+    template_name = 'users/register.html'
+
+
+# Контроллер редактирования пользователя
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:profile')
+
+    # def get_object(self, queryset=None):
+    #     return self.request.user
+
+
+class UserListView(ListView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:user_list')
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('users:user_list')
